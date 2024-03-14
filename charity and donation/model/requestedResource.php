dc1f2c283f6c06c5d604bd3716eb3e9813e5c358
Avizit Roy <contact@avizitrx.com>
2024-03-14 22:33:14 +0600

    fix: educational resources

--- charity and donation/model/requestedResource.php
+++ charity and donation/model/requestedResource.php
@@ -3,7 +3,7 @@
 
     function add_request_resource($resource_title,$thumbnail,$description,$category,$owner){
         $conn = dbConnection();
-        $sql = "INSERT INTO educational_resources (resource_title,thumbnail,description,category,owner) values('{$resource_title}','{$thumbnail}','{$description}','{$category}','{$owner}')";
+        $sql = "INSERT INTO educational_resources (resource_title,thumbnail,description,category,owner) values ('{$resource_title}','{$thumbnail}','{$description}','{$category}','{$owner}')";
         $result = mysqli_query($conn,$sql);
         if($result){
             return true;
